c012f7b2bcafc67133231edc76217cd0bdbc83dd
David Raleigh <davidraleigh@gmail.com>
2018-03-10 11:01:46 -0800

    Test_Cut

--- geometry-test/main.cpp
+++ geometry-test/main.cpp
@@ -27,6 +27,45 @@ namespace {
         EXPECT_NE(1, -1);
     }
 
+    TEST_F(GeometryClientTest, TEST_CUT_1){
+        std::shared_ptr<grpc::Channel> channel;
+        if (const char* env_p = std::getenv("GEOMETRY_SERVICE_HOST")) {
+            channel = grpc::CreateChannel(env_p, grpc::InsecureChannelCredentials());
+        } else {
+            channel = grpc::CreateChannel("localhost:8980", grpc::InsecureChannelCredentials());
+        }
+        std::unique_ptr<geometry::GeometryOperators::Stub> geometry_stub = geometry::GeometryOperators::NewStub(channel);
+
+        geometry::GeometryBagData serviceGeometry;
+        const char* wkt = "MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)), ((20 35, 45 20, 30 5, 10 10, 10 30, 20 35), (30 20, 20 25, 20 15, 30 20)))";
+        serviceGeometry.add_geometry_strings(wkt);
+        serviceGeometry.set_geometry_encoding_type(geometry::GeometryEncodingType::wkt);
+
+        geometry::GeometryBagData cutterGeometry;
+        const char* wkt_cutter = "LINESTRING(0 0, 45 45)";
+        cutterGeometry.add_geometry_strings(wkt_cutter);
+        cutterGeometry.set_geometry_encoding_type(geometry::GeometryEncodingType::wkt);
+
+        auto* operatorRequest = new geometry::OperatorRequest();
+        operatorRequest->mutable_left_geometry_bag()->CopyFrom(serviceGeometry);
+        operatorRequest->mutable_right_geometry_bag()->CopyFrom(cutterGeometry);
+        operatorRequest->set_operator_type(geometry::ServiceOperatorType::Cut);
+        operatorRequest->set_results_encoding_type(geometry::GeometryEncodingType::wkt);
+
+        auto* clientContext = new grpc::ClientContext();
+        auto* operatorResult = new geometry::OperatorResult();
+
+        geometry_stub->ExecuteOperation(clientContext, *operatorRequest, operatorResult);
+
+        std::string result1 = operatorResult->geometry_bag().geometry_strings(0);
+        std::string result2 = operatorResult->geometry_bag().geometry_strings(1);
+        const char* expected1 = "MULTIPOLYGON (((35.625 35.625, 40 40, 20 45, 35.625 35.625)), ((10 10, 20 20, 20 25, 23.333333333333336 23.333333333333336, 29.375 29.375, 20 35, 10 30, 10 10)))";
+        const char* expected2 = "MULTIPOLYGON (((40 40, 35.625 35.625, 45 30, 40 40)), ((30 5, 45 20, 29.375 29.375, 23.333333333333336 23.333333333333336, 30 20, 20 15, 20 20, 10 10, 30 5)))";
+
+        EXPECT_STREQ(expected1 , result1.c_str());
+        EXPECT_STREQ(expected2 , result2.c_str());
+    }
+
     TEST_F(GeometryClientTest, TEST_2) {
         std::shared_ptr<grpc::Channel> channel;
         if (const char* env_p = std::getenv("GEOMETRY_SERVICE_HOST")) {
@@ -34,8 +73,8 @@ namespace {
         } else {
             channel = grpc::CreateChannel("localhost:8980", grpc::InsecureChannelCredentials());
         }
-
         std::unique_ptr<geometry::GeometryOperators::Stub> geometry_stub = geometry::GeometryOperators::NewStub(channel);
+
         geometry::SpatialReferenceData spatialReferenceWGS84;
         spatialReferenceWGS84.set_wkid(4326);
 
