f2577fc9ca8968dfb2256dc9feeb33c14a227a84
David Raleigh <davidraleigh@gmail.com>
2018-12-28 14:13:21 -0800

    updates for passing tests

--- geometry-test/main.cpp
+++ geometry-test/main.cpp
@@ -15,6 +15,7 @@
 #include <cstdlib>
 #include <memory>
 
+
 namespace {
     class GeometryClientTest : public ::testing::Test {
     protected:
@@ -34,31 +35,31 @@ namespace {
         } else {
             channel = grpc::CreateChannel("localhost:8980", grpc::InsecureChannelCredentials());
         }
-        std::unique_ptr<geometry::GeometryOperators::Stub> geometry_stub = geometry::GeometryOperators::NewStub(channel);
+        std::unique_ptr<epl::geometry::GeometryOperators::Stub> geometry_stub = epl::geometry::GeometryOperators::NewStub(channel);
 
-        geometry::GeometryBagData serviceGeometry;
+        epl::geometry::GeometryBagData serviceGeometry;
         const char* wkt = "MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)), ((20 35, 45 20, 30 5, 10 10, 10 30, 20 35), (30 20, 20 25, 20 15, 30 20)))";
-        serviceGeometry.add_geometry_strings(wkt);
-        serviceGeometry.set_geometry_encoding_type(geometry::GeometryEncodingType::wkt);
+        serviceGeometry.add_wkt(wkt);
+        serviceGeometry.set_geometry_encoding_type(epl::geometry::GeometryEncodingType::wkt);
 
-        geometry::GeometryBagData cutterGeometry;
+        epl::geometry::GeometryBagData cutterGeometry;
         const char* wkt_cutter = "LINESTRING(0 0, 45 45)";
-        cutterGeometry.add_geometry_strings(wkt_cutter);
-        cutterGeometry.set_geometry_encoding_type(geometry::GeometryEncodingType::wkt);
+        cutterGeometry.add_wkt(wkt_cutter);
+        cutterGeometry.set_geometry_encoding_type(epl::geometry::GeometryEncodingType::wkt);
 
-        auto* operatorRequest = new geometry::OperatorRequest();
+        auto* operatorRequest = new epl::geometry::OperatorRequest();
         operatorRequest->mutable_left_geometry_bag()->CopyFrom(serviceGeometry);
         operatorRequest->mutable_right_geometry_bag()->CopyFrom(cutterGeometry);
-        operatorRequest->set_operator_type(geometry::ServiceOperatorType::Cut);
-        operatorRequest->set_results_encoding_type(geometry::GeometryEncodingType::wkt);
+        operatorRequest->set_operator_type(epl::geometry::ServiceOperatorType::Cut);
+        operatorRequest->set_results_encoding_type(epl::geometry::GeometryEncodingType::wkt);
 
         auto* clientContext = new grpc::ClientContext();
-        auto* operatorResult = new geometry::OperatorResult();
+        auto* operatorResult = new epl::geometry::OperatorResult();
 
         geometry_stub->ExecuteOperation(clientContext, *operatorRequest, operatorResult);
 
-        std::string result1 = operatorResult->geometry_bag().geometry_strings(0);
-        std::string result2 = operatorResult->geometry_bag().geometry_strings(1);
+        std::string result1 = operatorResult->geometry_bag().wkt(0);
+        std::string result2 = operatorResult->geometry_bag().wkt(1);
         const char* expected1 = "MULTIPOLYGON (((35.625 35.625, 40 40, 20 45, 35.625 35.625)), ((10 10, 20 20, 20 25, 23.333333333333336 23.333333333333336, 29.375 29.375, 20 35, 10 30, 10 10)))";
         const char* expected2 = "MULTIPOLYGON (((40 40, 35.625 35.625, 45 30, 40 40)), ((30 5, 45 20, 29.375 29.375, 23.333333333333336 23.333333333333336, 30 20, 20 15, 20 20, 10 10, 30 5)))";
 
@@ -73,35 +74,35 @@ namespace {
         } else {
             channel = grpc::CreateChannel("localhost:8980", grpc::InsecureChannelCredentials());
         }
-        std::unique_ptr<geometry::GeometryOperators::Stub> geometry_stub = geometry::GeometryOperators::NewStub(channel);
+        std::unique_ptr<epl::geometry::GeometryOperators::Stub> geometry_stub = epl::geometry::GeometryOperators::NewStub(channel);
 
-        geometry::SpatialReferenceData spatialReferenceWGS84;
+        epl::geometry::SpatialReferenceData spatialReferenceWGS84;
         spatialReferenceWGS84.set_wkid(4326);
 
-        auto* spatialReferenceCalif = new geometry::SpatialReferenceData();
+        auto* spatialReferenceCalif = new epl::geometry::SpatialReferenceData();
         spatialReferenceCalif->set_wkid(32632);
 
         // allocating this here means it is not copied in the set_allocated method, but a strange rule of control is given to the
         // operator request message
-        auto* serviceGeometry = new geometry::GeometryBagData();
+        auto* serviceGeometry = new epl::geometry::GeometryBagData();
         const char* wkt = "MULTILINESTRING ((500000       0, 400000  100000, 600000 -100000))";
-        serviceGeometry->add_geometry_strings(wkt);
-        serviceGeometry->set_geometry_encoding_type(geometry::GeometryEncodingType::wkt);
+        serviceGeometry->add_wkt(wkt);
+        serviceGeometry->set_geometry_encoding_type(epl::geometry::GeometryEncodingType::wkt);
         serviceGeometry->set_allocated_spatial_reference(spatialReferenceCalif);
 
-        auto* operatorRequest = new geometry::OperatorRequest();
+        auto* operatorRequest = new epl::geometry::OperatorRequest();
         operatorRequest->mutable_result_spatial_reference()->CopyFrom(spatialReferenceWGS84);
         operatorRequest->set_allocated_left_geometry_bag(serviceGeometry);
         operatorRequest->set_allocated_operation_spatial_reference(spatialReferenceCalif);
-        operatorRequest->set_operator_type(geometry::ServiceOperatorType::Project);
-        operatorRequest->set_results_encoding_type(geometry::GeometryEncodingType::wkt);
+        operatorRequest->set_operator_type(epl::geometry::ServiceOperatorType::Project);
+        operatorRequest->set_results_encoding_type(epl::geometry::GeometryEncodingType::wkt);
 
         auto* clientContext = new grpc::ClientContext();
-        auto* operatorResult = new geometry::OperatorResult();
+        auto* operatorResult = new epl::geometry::OperatorResult();
 
         geometry_stub->ExecuteOperation(clientContext, *operatorRequest, operatorResult);
 
-        std::string result = operatorResult->geometry_bag().geometry_strings(0);
+        std::string result = operatorResult->geometry_bag().wkt(0);
 
         const char* expected = "MULTILINESTRING ((9 0, 8.101251062924646 0.904618578893133, 9.898748937075354 -0.904618578893133))";
         EXPECT_STREQ(expected , result.c_str());
